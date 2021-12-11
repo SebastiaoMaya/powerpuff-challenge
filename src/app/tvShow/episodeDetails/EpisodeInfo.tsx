d43e373abc8f4312bfd99ff727275a26dd50b853
Sebastião Maya <sebastiao.rmvm@gmail.com>
2021-12-11 23:17:14 +0000

    Removed unused libs

--- src/app/tvShow/episodeDetails/EpisodeInfo.tsx
+++ src/app/tvShow/episodeDetails/EpisodeInfo.tsx
@@ -11,33 +11,30 @@ export const EpisodeInfo: React.FC<Episode> = ({
   url,
   summary,
   image,
-}) => {
-  console.log(summary);
-  return (
-    <Stack flexDir="column">
-      <Stack flexDir="row" alignItems="center">
-        <Text fontSize={["lg", "7xl"]} pr={2}>
-          {name}
-        </Text>
-        {url && (
-          <IconButton
-            colorScheme="blue"
-            aria-label="Open episode url"
-            icon={<ExternalLinkIcon />}
-            onClick={() => window.open(url, "_blank")}
-          />
-        )}
-      </Stack>
-      <Text
-        dangerouslySetInnerHTML={{
-          __html:
-            summary && summary.length !== 0
-              ? summary
-              : i18next.t("tvshow:episodeInfo.noSummary"),
-        }}
-        fontSize={["md", "lg"]}
-      />
-      <Image src={image?.medium} w={"50%"} />
+}) => (
+  <Stack flexDir="column">
+    <Stack flexDir="row" alignItems="center">
+      <Text fontSize={["lg", "7xl"]} pr={2}>
+        {name}
+      </Text>
+      {url && (
+        <IconButton
+          colorScheme="blue"
+          aria-label="Open episode url"
+          icon={<ExternalLinkIcon />}
+          onClick={() => window.open(url, "_blank")}
+        />
+      )}
     </Stack>
-  );
-};
+    <Text
+      dangerouslySetInnerHTML={{
+        __html:
+          summary && summary.length !== 0
+            ? summary
+            : i18next.t("tvshow:episodeInfo.noSummary"),
+      }}
+      fontSize={["md", "lg"]}
+    />
+    <Image src={image?.medium} w={"50%"} />
+  </Stack>
+);
